b456042f889b2a105c1566799dfd8e155d732e60
蒼一朗 石井 <h1eq1ta@gmail.com>
2025-12-27 22:18:04 +0900

    偶数か奇数かわかるよ if文はないよw

--- gitTest/main.cpp
+++ gitTest/main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+#include<string>
+using namespace std;
 
 int main() {
-	std::cout << "aaaaaa" << std::endl;
+	uint64_t max;
+	std::string o;
+	cout << "どこまで表示しますか?:";
+	cin >> max;
+	for (uint64_t i = 0; i <= max; i++) {
+		o += to_string(i) + "は";
+		bool isEven = (i % 2) == 0;
+		o += isEven ? "偶数です。" : "奇数です。";
+		o += '\n';
+	}
+	cout << o;
+	cout << "なんか押したら終わるよ:";
 	system("pause>0");
 }
